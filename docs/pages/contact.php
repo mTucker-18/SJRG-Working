8f190b1a6b5479d1bfa1607d88566c7f2190006a
Matthew Tucker <matthewt263@gmail.com>
2019-10-01 18:40:42 -0700

    try #3 :(

--- docs/pages/contact.php
+++ docs/pages/contact.php
@@ -3,18 +3,3 @@ if($_POST["message"]) {
     mail("matthewt263@gmail.com", "Form to email message", $_POST["message"], "From: an@email.address");
 }
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-  <head>
-  </head>
-    <body>
-
-        <form method="post" action="contact.php">
-            <textarea name="message"></textarea>
-            <input type="submit">
-        </form>
-
-    </body>
-</html>
